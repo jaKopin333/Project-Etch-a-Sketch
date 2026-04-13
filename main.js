293a7891072bbec9bf12592a42d1efc0f9d3df74
jaKopin333 <jacopelandavlnc@gmail.com>
2026-04-13 16:36:56 -0400

    Debugged and edited code. divs in html to js were not lining up correctly. Wasn't able to produce squares. Removed square:hover from CSS since it was fighting with JS eventlistener - mouseover. Edited #gridContainer to display flex and wrap. Was set to CSS grid with height by width at first as something I wanted to test out. The project ask for more flexbox. I was also running into the issue of where to have my functional math for squareSize as it was outside the correct function. It needed to be in the function Grids(size). Lastly my CSS flow was off and screwing with my hovering effect of my grids. Everything is in working order now.

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 function Grids(size){
     const container = document.getElementById('gridContainer');
+    const squareSize = 800 / size;
     container.innerHTML = ''; //clear existing grid
     for (let i = 0; i < size * size; i++) {
         const square = document.createElement('div');
+        square.style.width = squareSize + 'px';
+        square.style.height = squareSize + 'px';
         square.className = 'square';
         square.addEventListener('mouseover', () => {
             square.style.backgroundColor = 'black'; //Changes color on hover 
@@ -14,8 +17,9 @@ function Grids(size){
 document.getElementById('newGridButton').addEventListener('click', () =>
 {
     let size = prompt("Enter the number of squares per side (max 100):");
-    size = Math.min(size, 100); // limit input to 100
+    size = Number(size); // limit input to 100
     Grids(size);
 });
 
 Grids(16);
+
